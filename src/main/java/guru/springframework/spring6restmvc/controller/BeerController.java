597f8bb069d31cb299883bad4dfedae39ba3b88d
ErhanBatu <e.batu44@icloud.com>
2023-04-16 03:46:41 +0200

    add http get with spring mvc list operation

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,16 +5,29 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
+
+//my api http://api.springframework.guru/api/v1/beer
+//rest controller will get the result from api
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
 
     //normally i have to create a const for this but i am using @AllArgsConstructor
     private final BeerService beerService;
+
+    //first you have to run Spring6RestMvcApplication and after that in postman "http://localhost:8080/api/v1/beer" you will see your data
+    //requestmapping will convert it json format
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id){
 
--- src/main/java/guru/springframework/spring6restmvc/model/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/model/Beer.java
@@ -19,6 +19,6 @@ public class Beer {
     private String upc;
     private Integer quantityOnHand;
     private BigDecimal price;
-    private LocalDateTime createDate;
+    private LocalDateTime createdDate;
     private LocalDateTime updateDate;
 }
