a0e5f78d3074029aeb8398650791a1286ceaed62
ErhanBatu <e.batu44@icloud.com>
2023-04-16 02:46:56 +0200

    add @data and @builder

---
+++ src/main/java/guru/springframework/spring6restmvc/model/Beer.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6restmvc.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+//data returns getter setter const hashcode tostring all
+//builder will give you in the implementation of the service Beer.builder().build();
+@Builder
+@Data
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+}
