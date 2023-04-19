5dee5005dce1551230a5346c015d7347bb92f918
ErhanBatu <e.batu44@icloud.com>
2023-04-19 19:20:57 +0200

    87. Using ResponseStatus Annotation

--- src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
@@ -5,15 +5,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 //ControllerAdvice: it will picked up globally you can use it everywhere
-@ControllerAdvice
-public class ExceptionController {
-
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity handleNotFoundException(){
-
-        System.out.println("in exception handler");
-
-        return ResponseEntity.notFound().build();
-    }
-
-}
+//you don't need this class anymore because i put @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "value not found")
+//in the NotFoundException class
+//@ControllerAdvice
+//public class ExceptionController {
+//
+//    @ExceptionHandler(NotFoundException.class)
+//    public ResponseEntity handleNotFoundException(){
+//
+//        System.out.println("in exception handler");
+//
+//        return ResponseEntity.notFound().build();
+//    }
+//
+//}
--- src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
@@ -1,6 +1,10 @@
 package guru.springframework.spring6restmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 //RuntimeException is standart exception
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "value not found")
 public class NotFoundException extends RuntimeException{
 
     //i implement default consts
