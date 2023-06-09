7a162be0f5c0891092b7f059e50842cadad0f1a2
Erhan <e.batu44@icloud.com>
2023-06-09 20:03:18 +0200

    63. HTTP POST with Spring MVC

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,11 +4,10 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +23,17 @@ public class BeerController {
     //Normally I have to create an const for that but I use AllArgsConst
     private final BeerService beerService;
 
+    //POST
+    @PostMapping
+    //RequestBody it is gonna bind the JSON with the beer object
+    public ResponseEntity handlePost(@RequestBody Beer beer){
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    //GET
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
 
@@ -31,6 +41,7 @@ public class BeerController {
 
     }
 
+    //GET
     @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
--- src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
@@ -2,5 +2,5 @@ package guru.springframework.spring6restmvc.model;
 
 public enum BeerStyle {
 
-    PALE_ALE
+    PALE_ALE, ALE
 }
