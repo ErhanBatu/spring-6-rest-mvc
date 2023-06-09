73d3541798bbb6489a42d0ad5605399acd774a67
Erhan <e.batu44@icloud.com>
2023-06-09 19:22:41 +0200

    61. Using Path Parameters - Get By Id

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,7 +5,9 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,23 +18,25 @@ import java.util.UUID;
 @AllArgsConstructor
 //Restcontroller retuns you response body as json, this is gonna convert to JSON
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     //Normally I have to create an const for that but I use AllArgsConst
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
 
         return beerService.listBeers();
 
     }
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
         log.debug("Get Beer by id in controller, thanks to Slf4j I can write this log");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 
 }
